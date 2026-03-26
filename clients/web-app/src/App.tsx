98b86987a78c94597243e7b13d1c2d1d483df05f
Angie Roa <a.roap@uniandes.edu.co>
2026-03-26 17:30:17 -0500

    feat: add filters (#7)

--- clients/web-app/src/App.tsx
+++ clients/web-app/src/App.tsx
@@ -1,13 +1,23 @@
-import Background from '@/components/Background'
-import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Background from "@/components/Background";
+import { SearchProvider } from "@/context/SearchContext";
+import Home from "./pages/Home";
+import SearchResults from "./pages/SearchResults";
 
 function App() {
   return (
-    <div className="relative min-h-screen">
-      <Background />
-      <Home />
-    </div>
-  )
+    <BrowserRouter>
+      <SearchProvider>
+        <div className="relative min-h-screen">
+          <Background />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchResults />} />
+          </Routes>
+        </div>
+      </SearchProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
