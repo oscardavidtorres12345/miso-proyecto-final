1b0dbb288dc0f24ed8d4e3f9cb57fffc11112175
Esteban Heredia <e.herediar@uniandes.edu.co>
2026-04-09 18:30:35 -0500

    make inventory equal as info in search service

--- infrastructure/docker/inventory-db-init/01_seed_inventory_stock.sql
+++ infrastructure/docker/inventory-db-init/01_seed_inventory_stock.sql
@@ -1,15 +1,14 @@
 -- Seed inicial para inventory_db
--- IDs de room alineados con search seed: 101, 102, 201
-
+-- Alineado con search-db-init/02_seed.sql:
+-- - 25 propiedades x 3 rooms => room_id secuenciales 1..75
+-- - inventario para 10 dias: CURRENT_DATE + 0..9
 INSERT INTO inventory_stock (room_id, date, total_units, confirmed_units, held_units)
-VALUES
-    (101, DATE '2026-04-10', 3, 0, 0),
-    (101, DATE '2026-04-11', 3, 0, 0),
-    (101, DATE '2026-04-12', 3, 0, 0),
-    (102, DATE '2026-04-10', 2, 0, 0),
-    (102, DATE '2026-04-11', 2, 0, 0),
-    (102, DATE '2026-04-12', 2, 0, 0),
-    (201, DATE '2026-04-10', 4, 0, 0),
-    (201, DATE '2026-04-11', 4, 0, 0),
-    (201, DATE '2026-04-12', 4, 0, 0)
+SELECT
+    r.room_id,
+    CURRENT_DATE + d.day_offset,
+    5 AS total_units,
+    0 AS confirmed_units,
+    0 AS held_units
+FROM generate_series(1, 75) AS r(room_id)
+CROSS JOIN generate_series(0, 9) AS d(day_offset)
 ON CONFLICT (room_id, date) DO NOTHING;
